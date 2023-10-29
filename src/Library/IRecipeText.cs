d5756798eb1e1511b943d0632882297be9701e2b
belentellechea <belen.tellechea@correo.ucu.edu.uy>
2023-10-29 12:34:14 -0300

    comentarios

--- src/Library/IRecipeText.cs
+++ src/Library/IRecipeText.cs
@@ -1,5 +1,9 @@
 namespace Full_GRASP_And_SOLID
 {
+    /*
+    para eliminar la dependencia entre la clase ConsolePrinter con la clase Recipe, se debe generar una abstracción entre ellas 
+    con respecto al método que ambas implementan. Para esto, se aplica el principio DIP (Dependency Inversion Principle)
+    */
     public interface IRecipeText
     {
         string GetTextToPrint();
